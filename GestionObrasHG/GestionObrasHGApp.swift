d53c1eb96cdadd3a6f0d03659e41b3b7bbaba475
sherreratecnicos-cloud <sherrera.tecnicos@gmail.com>
2025-10-28 23:36:26 +0100

    Update GestionObrasHGApp.swift

--- GestionObrasHG/GestionObrasHGApp.swift
+++ GestionObrasHG/GestionObrasHGApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct GestionObrasHGApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
